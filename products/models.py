a3e6f1506ad7ecdae3d90342a0da024e861c2a69
srikant <srikantkanekar01@gmail.com>
2020-04-24 18:20:35 +0530

    'f'

--- products/models.py
+++ products/models.py
@@ -6,13 +6,14 @@ from django.utils.text import slugify
 class Product(models.Model):
     title = models.CharField(max_length=100)
     category = TreeForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
-    price = models.FloatField()
-    description = models.TextField()
     image_url_1 = models.ImageField(upload_to='product/img', null=True, blank=True)
     image_url_2 = models.ImageField(upload_to='product/img', null=True, blank=True)
     image_url_3 = models.ImageField(upload_to='product/img', null=True, blank=True)
-    quantity_left = models.IntegerField()
-    return_days = models.IntegerField()
+    offer_price = models.FloatField(null=True, blank=True)
+    original_price = models.FloatField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    quantity_left = models.IntegerField(null=True, blank=True)
+    return_days = models.IntegerField(null=True, blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
     def save(self, *args, **kwargs):
@@ -27,7 +28,7 @@ class Category(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True, on_delete=models.CASCADE)
     image_url = models.ImageField(upload_to='category/img', null=True, blank=True)
-    description = models.CharField(max_length=100, null=True, blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
     class MPTTMeta:
