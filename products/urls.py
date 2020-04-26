c418bd1a25a98da4c7c2a74ca6b0dae1a101a7c0
srikant <srikantkanekar01@gmail.com>
2020-04-26 09:05:52 +0530

    'g'

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('<slug:slug>/', views.show_category, name='category'),
-    path('<slug:slug>/', views.show_category, name='show_category'),
-    path('<slug:slug>', views.product_detail, name='product_detail'),
+    path('Category/<slug:slug>/', views.show_category, name='category'),
+    path('Category/<slug:slug>/', views.show_category, name='show_category'),
+    path('Product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('faq/', views.faq, name='faq'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('cart/', views.Cart, name='cart'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('order_track/', views.order_track, name='order_track'),
 ]
--- products/views.py
+++ products/views.py
@@ -16,3 +16,24 @@ def show_category(request, slug):
 def product_detail(request, slug):
     product = Product.objects.get(slug=slug)
     return render(request, 'template/product_detail.html', {'product': product})
+
+
+def faq(request):
+    return render(request, 'template/faq.html')
+
+
+def contact_us(request):
+    return render(request, 'template/Contact us.html')
+
+
+def Cart(request):
+    return render(request, 'template/Cart.html')
+
+
+def order_track(request):
+    return render(request, 'template/Order track.html')
+
+
+def about_us(request):
+    return render(request, 'template/About us.html')
+
