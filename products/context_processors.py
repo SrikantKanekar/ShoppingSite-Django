7f79d0bb12ebf5f6664f9eaa422331a800e37628
srikant <srikantkanekar01@gmail.com>
2020-05-21 12:59:33 +0530

    's'

--- products/context_processors.py
+++ products/context_processors.py
@@ -2,11 +2,12 @@ from .models import Category, Product, Profile, Comment, Admin
 
 
 def add_variable_to_context(request):
-    wishlist_count = Profile.objects.get(user=request.user).wishlist_products.all().count()
-    cart_count = Profile.objects.get(user=request.user).cart_products.all().count()
-    order_count = Admin.objects.filter(customer=request.user).count()
-    return {
-        'order_count': order_count,
-        'wishlist_count': wishlist_count,
-        'cart_count': cart_count,
-    }
+    if request.user.is_authenticated:
+        wishlist_count = Profile.objects.get(user=request.user).wishlist_products.all().count()
+        cart_count = Profile.objects.get(user=request.user).cart_products.all().count()
+        order_count = Admin.objects.filter(customer=request.user).count()
+        return {
+            'order_count': order_count,
+            'wishlist_count': wishlist_count,
+            'cart_count': cart_count,
+        }
